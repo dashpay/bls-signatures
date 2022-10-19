5b8d33ea1c3c3bb01adf7382195c6162356bf9f8
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2022-10-19 12:30:01 +0400

    fix the bundle script

--- js_build.sh
+++ js_build.sh
@@ -1,9 +1,18 @@
-#!/usr/bin/env bash
+DOCKER_IMAGE=emscripten/emsdk
 
 git submodule update --init --recursive
-
 mkdir js_build
-cd js_build
 
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=$(dirname $(realpath $(which emcc)))/cmake/Modules/Platform/Emscripten.cmake
-cmake --build . --
+docker run \
+  --rm \
+  -v $(pwd):/src \
+  -w="/src/js_build" \
+  $DOCKER_IMAGE \
+  emcmake cmake /src
+
+docker run \
+  --rm \
+  -v $(pwd):/src \
+  -w="/src/js_build" \
+  $DOCKER_IMAGE \
+  cmake --build . -- -j10
