77fc6d7d311bbad1320ebab2bb127bb94a2427f3
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2023-01-30 16:52:12 +0300

    testings

--- js_build.sh
+++ js_build.sh
@@ -1,18 +1,9 @@
-DOCKER_IMAGE=emscripten/emsdk
+#!/usr/bin/env bash
 
 git submodule update --init --recursive
+
 mkdir js_build
+cd js_build
 
-docker run \
-  --rm \
-  -v $(pwd):/src \
-  -w="/src/js_build" \
-  $DOCKER_IMAGE \
-  emcmake cmake /src
-
-docker run \
-  --rm \
-  -v $(pwd):/src \
-  -w="/src/js_build" \
-  $DOCKER_IMAGE \
-  cmake --build . -- -j10
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=$(realpath $(which emcc))/cmake/Modules/Platform/Emscripten.cmake
+cmake --build . --
